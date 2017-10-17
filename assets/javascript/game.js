980b2cbe8c0a25d498440dbfe007600531e32173
pablackhawk <l.tirtarahardja@gmail.com>
2017-10-16 18:22:46 -0700

    game logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,27 +3,52 @@ var computerChoices = ["a", "b", "c", "d", "e","f",
 					   "m", "n", "o", "p", "q", "r", "s",
 					   "t", "u", "v", "w", "x", "y", "z"];
 
+var incorrectGuess = []
+
 var correct = 0;
 var incorrect = 0;
+var choices = 10;
+
+alert("Make a selection! Guess what I have chosen!");
+
+function gameReset() {
+	choices = 10;
+	correct = 0;
+	incorrect = 0;
+	incorrectGuess = [];
+	alert("Try Again");
+}
+
 
 document.onkeyup = function(event) {
 	var userGuess = event.key;
-	var computerGuess = computerChoices[Math.floor(Math.random () * computerChoices.length)];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	
 
-
-	if(userGuess === computerGuess){
-		correct++
+	if(choices > 1){
+		if(userGuess === computerGuess){
+			correct++;
+			incorrectGuess = [];
+		}
+		else {
+			choices--;
+			incorrect++;
+			incorrectGuess.push(userGuess);
+		}
 	}
+
 	else {
-		incorrect++
-	}
+		gameReset();
+		}
 
 
 	var html ="<h1>Make a selection! Guess what I have chosen!</h1>" +
 			"<p> You chose: " + userGuess + "</p>" +
 			"<p> I chose: " + computerGuess + "</p>" +
 			"<p> Correct: " + correct + "</p>" +
-			"<p> Incorrect: " + incorrect + "</p>"
+			"<p> Incorrect: " + incorrect + "</p>" +
+			"<p> Chances left: " + choices + "</p>" +
+			"<p> Your past guesses: " + incorrectGuess + "</p>"
 
 		document.querySelector("#game").innerHTML = html;
 }
